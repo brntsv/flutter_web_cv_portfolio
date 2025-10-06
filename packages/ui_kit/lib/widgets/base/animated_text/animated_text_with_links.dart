1fbcc4bcc7a4a8f2a41978ca723c708dc048d650
Nikita Barantsev <59911388+brntsv@users.noreply.github.com>
2025-10-06 21:14:26 +0700

    Рефакторинг AnimatedTextWithLinks

--- packages/ui_kit/lib/widgets/base/animated_text/animated_text_with_links.dart
+++ packages/ui_kit/lib/widgets/base/animated_text/animated_text_with_links.dart
@@ -27,145 +27,156 @@ class AnimatedTextWithLinks extends StatelessWidget with AnimationTimingMixin {
   final int appearClass;
 
   @override
-  Widget build(BuildContext context) => Text.rich(
-        TextSpan(children: _buildAnimatedTextSpans(context)),
-      );
-
-  List<InlineSpan> _buildAnimatedTextSpans(BuildContext context) {
+  Widget build(BuildContext context) {
     final textStyle = textStyles(context);
     final color = colors(context);
-    final spans = <InlineSpan>[];
 
+    final spans = <InlineSpan>[];
     final linkRegex = RegExp(r'\[(.*?)\]\((.*?)\)');
     var lastMatchEnd = 0;
     var wordIndex = 0;
 
-    // Обработка ссылок
     for (final match in linkRegex.allMatches(text)) {
-      // Добавляем текст до ссылки
       if (match.start > lastMatchEnd) {
         final beforeLink = text.substring(lastMatchEnd, match.start);
         if (beforeLink.isNotEmpty) {
-          spans.addAll(
-            _buildWordsSpans(
-              beforeLink,
-              textStyle.baseText,
-              wordIndex,
-              context,
-            ),
-          );
-          wordIndex += beforeLink.split(' ').length;
+          final words = beforeLink.split(' ');
+          for (var i = 0; i < words.length; i++) {
+            final word = words[i];
+            final isLastWord = i == words.length - 1;
+            spans.add(
+              WidgetSpan(
+                child: AnimatedWord(
+                  word: '$word${isLastWord ? '' : ' '}',
+                  style: textStyle.baseText,
+                  wordIndex: wordIndex + i,
+                  appearClass: appearClass,
+                  appearDuration: appearDuration,
+                ),
+              ),
+            );
+          }
+          wordIndex += words.length;
         }
       }
 
-      // Добавляем саму ссылку (как отдельное слово)
       final linkText = match.group(1)!;
       final linkUrl = match.group(2)!;
-
       spans.add(
-        _buildAnimatedWordWidget(
-          linkText,
-          textStyle.baseText.copyWith(color: color.blue),
-          wordIndex,
-          linkUrl,
-          context,
+        WidgetSpan(
+          child: AnimatedWord(
+            word: linkText,
+            style: textStyle.baseText.copyWith(color: color.blue),
+            wordIndex: wordIndex,
+            appearClass: appearClass,
+            appearDuration: appearDuration,
+            linkUrl: linkUrl,
+          ),
         ),
       );
       wordIndex++;
-
       lastMatchEnd = match.end;
     }
 
-    // Добавляем оставшийся текст после последней ссылки
     if (lastMatchEnd < text.length) {
       final afterLinks = text.substring(lastMatchEnd);
       if (afterLinks.isNotEmpty) {
-        spans.addAll(
-          _buildWordsSpans(
-            afterLinks,
-            textStyle.baseText,
-            wordIndex,
-            context,
-          ),
-        );
+        final words = afterLinks.split(' ');
+        for (var i = 0; i < words.length; i++) {
+          final word = words[i];
+          final isLastWord = i == words.length - 1;
+          spans.add(
+            WidgetSpan(
+              child: AnimatedWord(
+                word: '$word${isLastWord ? '' : ' '}',
+                style: textStyle.baseText,
+                wordIndex: wordIndex + i,
+                appearClass: appearClass,
+                appearDuration: appearDuration,
+              ),
+            ),
+          );
+        }
+        // wordIndex increment is not necessary beyond final rendering
       }
     }
 
-    return spans;
+    return Text.rich(TextSpan(children: spans));
   }
+}
 
-  List<InlineSpan> _buildWordsSpans(
-    String text,
-    TextStyle style,
-    int startWordIndex,
-    BuildContext context,
-  ) {
-    final words = text.split(' ');
-    final spans = <InlineSpan>[];
+/// {@template animated_word.class}
+/// Анимированное слово
+/// {@endtemplate}
+class AnimatedWord extends StatelessWidget with AnimationTimingMixin {
+  /// {@macro animated_word.class}
+  const AnimatedWord({
+    required this.word,
+    required this.style,
+    required this.wordIndex,
+    required this.appearClass,
+    required this.appearDuration,
+    this.linkUrl,
+    super.key,
+  });
 
-    for (var i = 0; i < words.length; i++) {
-      final word = words[i];
-      final isLastWord = i == words.length - 1;
+  /// Слово
+  final String word;
 
-      spans.add(
-        _buildAnimatedWordWidget(
-          '$word${isLastWord ? "" : " "}',
-          style,
-          startWordIndex + i,
-          null,
-          context,
-        ),
-      );
-    }
+  /// Стиль текста
+  final TextStyle style;
 
-    return spans;
-  }
+  /// Индекс слова
+  final int wordIndex;
 
-  WidgetSpan _buildAnimatedWordWidget(
-    String word,
-    TextStyle style,
-    int wordIndex,
-    String? linkUrl,
-    BuildContext context,
-  ) {
-    // Вычисляем "класс" анимации (1-20) аналогично оригинальному AnimatedText
+  /// Класс анимации
+  final int appearClass;
+
+  /// Длительность анимации
+  final double appearDuration;
+
+  /// URL ссылки
+  final String? linkUrl;
+
+  @override
+  Widget build(BuildContext context) {
     final animationClass = getAnimationClass(wordIndex, appearClass);
     final animationParams = getAnimationParams(animationClass);
-
     final color = colors(context);
 
-    return WidgetSpan(
-      child: TweenAnimationBuilder<double>(
-        tween: Tween<double>(begin: 0, end: 1),
-        duration: Duration(milliseconds: (appearDuration * 1000).toInt()),
-        curve: Interval(
-          animationParams.startTime,
-          animationParams.endTime,
-          curve: Curves.easeOut,
-        ),
-        builder: (context, value, child) {
-          final animatedStyle = style.copyWith(
-            color: Color.lerp(
-              color.white,
-              style.color ?? color.black,
-              value,
-            ),
-          );
+    return TweenAnimationBuilder<double>(
+      tween: Tween<double>(begin: 0, end: 1),
+      duration: Duration(milliseconds: (appearDuration * 1000).toInt()),
+      curve: Interval(
+        animationParams.startTime,
+        animationParams.endTime,
+        curve: Curves.easeOut,
+      ),
+      builder: (context, value, child) {
+        final animatedStyle = style.copyWith(
+          color: Color.lerp(
+            color.white,
+            style.color ?? color.black,
+            value,
+          ),
+        );
 
-          if (linkUrl != null) {
-            return Text.rich(
+        if (linkUrl != null) {
+          return MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: Text.rich(
               TextSpan(
                 text: word,
                 style: animatedStyle,
                 recognizer: TapGestureRecognizer()
-                  ..onTap = () => UrlLauncher.openUrl(linkUrl),
+                  ..onTap = () => UrlLauncher.openUrl(linkUrl!),
               ),
-            );
-          }
+            ),
+          );
+        }
 
-          return Text(word, style: animatedStyle);
-        },
-      ),
+        return Text(word, style: animatedStyle);
+      },
     );
   }
 }
